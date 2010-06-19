1f234231025111c9e4e7bcb587730428717bff3d
Jacob Dam <ngocphuc@gmail.com>
2010-06-19 15:31:06 +0700

    generate railsvn blog app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '9a644e14f77a1e7ae8fd102b4a333801720c91e630a57a088c778985f75e6b45b72a35d23ca930038164d32769237cde1c22e337ef22d10dc5823260e35f3561'
