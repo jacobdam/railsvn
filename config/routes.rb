a1dadc413777a98c5a499568d9b90ec624ab8d74
Jacob Dam <ngocphuc@gmail.com>
2010-07-11 17:57:51 +0700

    force all requests should go through www subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Railsvn::Application.routes.draw do |map|
-  root :to => "homes#homepage"
+  constraints(:subdomain => /^$/) do
+    match "/", :to => redirect("http://www.#{APP_CONFIG[:domain]}/")
+    match "/*path", :to => redirect("http://www.#{APP_CONFIG[:domain]}/")
+  end
 
-  resources :articles
-  
-  devise_for :users
+  constraints(:subdomain => /www/) do
+    root :to => "homes#homepage"
+    resources :articles
+    devise_for :users
+  end
 end
