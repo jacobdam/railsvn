26cdc149345bee05c2b59883f4f8f3d5aef59004
Jacob Dam <ngocphuc@gmail.com>
2010-07-22 20:27:13 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Railsvn::Application.routes.draw do |map|
-  constraints(:subdomain => /^$/) do
-    match "/", :to => redirect("http://www.#{APP_CONFIG[:domain]}/")
-    match "/*path", :to => redirect("http://www.#{APP_CONFIG[:domain]}/")
-  end
-
   constraints(:subdomain => /www/) do
     root :to => "homes#homepage"
     resources :articles
     devise_for :users
   end
+
+  match "/", :to => redirect("http://www.#{APP_CONFIG[:domain]}/")
+  match "/*path", :to => redirect("http://www.#{APP_CONFIG[:domain]}/%{path}")
 end
