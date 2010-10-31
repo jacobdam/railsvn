c6bc734809d7c71c72ac307feeac02a6921d64cd
Jacob Dam <ngocphuc@gmail.com>
2010-10-31 16:35:43 +0700

    fix comment authorization bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
+class ActionUnauthorized < Exception; end
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
 
   def require_admin!
-    if !user_signed_in? || !current_user.admin?
-      flash[:error] = "You don't have permission to access this page"
-      redirect_to root_url
-    end
+    unauthorized! unless user_signed_in? && current_user.admin?
+  end
+
+  def unauthorized!(message = "You don't have permission to access this page")
+    raise ActionUnauthorized, message
+  end
+
+  rescue_from ActionUnauthorized do |e|
+    redirect_to root_url, :alert => e.message
   end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+
   def new
     @article = Article.find(params[:article_id])
     redirect_to article_path(@article), :alert => 'comment must posted in article'
@@ -17,10 +19,12 @@ class CommentsController < ApplicationController
 
   def edit
     @comment = Comment.find(params[:id])
+    require_admin_or_author!(@comment)
   end
 
   def update
     @comment = Comment.find(params[:id])
+    require_admin_or_author!(@comment)
     if @comment.update_attributes(params[:comment])
       redirect_to article_path(@comment.article), :notice => 'comment updated'
     else
@@ -30,8 +34,14 @@ class CommentsController < ApplicationController
   
   def destroy
     @comment = Comment.find(params[:id])
+    require_admin_or_author!(@comment)
     @comment.destroy
     flash[:notice] = "Successfully destroyed comment."
     redirect_to @comment.article
+  end
+
+private
+  def require_admin_or_author!(comment)
+    unthorized! unless user_signed_in? && (current_user.admin? || comment.author == current_user)
   end
 end
